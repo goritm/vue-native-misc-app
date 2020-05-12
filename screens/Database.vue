71dcf563a95e5a674fbc35c574283fa5a122792f
Jose Sandoval <joseasandovalp@gmail.com>
2020-05-12 13:04:50 -0500

    storage functionality

--- screens/Database.vue
+++ screens/Database.vue
@@ -1,38 +1,109 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">{{ message }}</text>
+  <view>
+    <view class="container">
+      <text-input class="text-input" v-model="text" />
+      <touchable-opacity class="button" :on-press="addDog">
+        <text class="button-text">Agregar</text>
+      </touchable-opacity>
+      <touchable-opacity class="button" :on-press="clearList">
+        <text class="button-text">Reset</text>
+      </touchable-opacity>
+    </view>
+    <view class="test" v-for="(dogo, index) in dogos" :key="index">
+      <touchable-opacity :on-press="() => removeDog(dogo)">
+        <text class="text-container">{{ dogo }}</text>
+      </touchable-opacity>
+    </view>
   </view>
 </template>
 
 <script>
-import * as SQLite from "expo-sqlite";
-
-// const db = SQLite.openDatabase("db");
-
-// db.exec([], false);
-
-// https://github.com/react-native-community/async-storage
-// https://medium.com/swlh/react-native-with-sqlite-1ec64702e35e
-// https://docs.expo.io/versions/latest/sdk/sqlite/
+import { AsyncStorage } from "react-native";
 
 export default {
   data() {
     return {
-      message: "Hello World",
+      text: "",
+      dogos: [],
     };
+  },
+  mounted() {
+    this.getDog();
+  },
+  methods: {
+    async addDog() {
+      try {
+        await AsyncStorage.setItem(this.text, this.text);
+        alert(this.text + " agregado!");
+        this.dogos.push(this.text);
+        this.text = "";
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    async getDog() {
+      try {
+        const keys = await AsyncStorage.getAllKeys();
+        // const items = await AsyncStorage.multiGet(keys);
+        this.dogos = keys;
+        console.log(this.dogos);
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    async removeDog(dogo) {
+      try {
+        await AsyncStorage.removeItem(dogo);
+        this.dogos = [];
+        this.getDog();
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    async clearList() {
+      try {
+        await AsyncStorage.clear();
+        this.dogos = [];
+        alert("Datos borrados");
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
   },
 };
 </script>
 
 <style>
 .container {
-  flex: 1;
-  background-color: white;
+  background-color: #333;
+  flex-direction: row;
+  padding: 20px;
+}
+.test {
   align-items: center;
   justify-content: center;
+  padding-top: 10;
 }
-.text-color-primary {
-  color: blue;
-  font-size: 30;
+.text-input {
+  background-color: white;
+  margin-right: 5px;
+  flex: 3;
+}
+.text-container {
+  color: black;
+  padding: 2;
+}
+.button {
+  flex: 1;
+  background-color: #008080;
+  margin-left: 5px;
+  align-items: center;
+  justify-content: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.button-text {
+  color: white;
+  font-weight: normal;
 }
 </style>
